1b455db27aa720bf484ad0c85e39ebe047da104a
E.L.3.G.O.S.T <EL3GOST@MacBook-Pro-de-EL3GOST.local>
2015-11-30 22:11:59 +0100

    Ajout d'un squelette basique de l'application

--- Eilya/app/src/main/java/groupeeilya/eilya/MainActivity.java
+++ Eilya/app/src/main/java/groupeeilya/eilya/MainActivity.java
@@ -1,16 +1,24 @@
 package groupeeilya.eilya;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText_Search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText_Search = (EditText) findViewById(R.id.editTextSearch);
     }
 
     @Override
@@ -33,5 +41,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void Btn_Search(View view)
+    {
+        String keyword = editText_Search.getText().toString();
+        editText_Search.setText(""); //Reset of the search bar
+        Toast.makeText(this, "Search with keyword : " + keyword, Toast.LENGTH_LONG).show();
+
+    }
+
+    public void Btn_OptionOnClick(View view)
+    {
+        Intent openOption = new Intent(this, Activity_Options.class);
+        startActivity(openOption);
+    }
+
+    public void Btn_FiltrageOnClick(View view)
+    {
+        Intent openFiltrage = new Intent(this, Activity_Filtrage.class);
+        startActivity(openFiltrage);
     }
 }
