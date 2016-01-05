f4d66518198310b294adbe5ae8ebf2a5e2f7732d
Tsuchi <bastien.auguste@gmail.com>
2016-01-05 17:22:33 +0100

    Première ébauche test de la vue des résultats de la recherche. Deuxième partie

---
+++ Eilya/app/src/main/java/groupeeilya/eilya/SearchResultsActivity.java
@@ -0,0 +1,25 @@
+package groupeeilya.eilya;
+
+import android.content.Intent;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+/**
+ * Created by bastien.auguste on 05/01/2016.
+ */
+/* Fonctionnement prévu :
+- Récupération du nombre1 de résultats de la recherche
+- Division de ce nombre1 par 3 Modulo 3 = result1.
+- Creer result1 LinearLayout Horizontal (ajoutés au LinearLayout Vertical)
+- Chacun de ces Linear Layout comprend 3 ImageView aux normes similaires à celle de l'exemple
+-
+ */
+public class SearchResultsActivity extends AppCompatActivity {
+
+}
